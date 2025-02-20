14e7f1fc70a209e5fbd6e476ca8a0fc17f74dc5c
yadao <yadaovinzce@gmail.com>
2025-02-19 20:09:44 -0500

    Completed POST through FLask

---
+++ Day 60/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def receive_data():
+    name = request.form['Name']
+    password = request.form['Password']
+    return f"<h1>Name: {name}, Password: {password}</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
